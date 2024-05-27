819dbd21ec1ee70f6be711b4f742e5739b6c97f9
221186 <jdyjsh77@naver.com>
2024-05-27 22:07:03 +0900

    feat: 페이지 공통 스타일 추가

---
+++ src/styles/PageLayout.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+  width: 100%;
+  height: 190vh;
+  background-color: #f1f1f1;
+`;
+export const Body = styled.body`
+  // 모든 페이지 기본 margin
+  padding: 4rem 20rem;
+`;
