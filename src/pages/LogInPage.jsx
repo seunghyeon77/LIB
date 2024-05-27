08ff11af35b0b1745d1309deff72d5ba5a271550
221186 <jdyjsh77@naver.com>
2024-05-27 12:24:10 +0900

    feat: 스타일 파일 추가

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
 import LoginIcons from "../components/LogIn/LoginIcons";
 import LoginForm from "../components/LogIn/LoginForm";
+import { LoginFormWrapper, LoginPageDiv } from "../styles/Login/LoginLayout";
 
 export default function LogInPage() {
   return (
@@ -12,14 +12,3 @@ export default function LogInPage() {
     </LoginPageDiv>
   );
 }
-const LoginPageDiv = styled.div`
-  display: flex;
-  overflow: hidden;
-`;
-const LoginFormWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
