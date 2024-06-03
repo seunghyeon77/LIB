dd1c7769ba33ad952ac390aea06ceb9b61e7c13e
221186 <jdyjsh77@naver.com>
2024-06-03 11:14:06 +0900

    feat: 책 상세 페이지 스타일 추가

--- src/pages/BookDetailPage.jsx
+++ src/pages/BookDetailPage.jsx
@@ -1,3 +1,18 @@
+import BookDetailAi from "../components/BookDetail/BookDetailAi";
+import BookDetailCommu from "../components/BookDetail/BookDetailCommu";
+import BookDetailContent from "../components/BookDetail/BookDetailContent";
+import BookDetailinfo from "../components/BookDetail/BookDetailInfo";
+import { Body, Page } from "../styles/PageLayout";
+
 export default function BookDetailPage() {
-  return <></>
+  return (
+    <Page style={{backgroundColor: '#ffffff'}}>
+      <Body>
+        <BookDetailinfo />
+        <BookDetailContent />
+        <BookDetailAi />
+        <BookDetailCommu />
+      </Body>
+    </Page>
+  )
 }
