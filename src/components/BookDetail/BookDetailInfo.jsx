3577d063a900e3c9370e6cb0e1a2926801b51dda
221186 <jdyjsh77@naver.com>
2024-06-04 11:06:32 +0900

    feat: 별점 기능 추가

--- src/components/BookDetail/BookDetailInfo.jsx
+++ src/components/BookDetail/BookDetailInfo.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import { BookInfoGenre, BookInfoTitle } from "../../styles/Main/BookStyle";
 import styled from "styled-components";
+import StarNumber from "../starNumber";
 
 export default function BookDetailinfo() {
   return (
@@ -9,28 +11,30 @@ export default function BookDetailinfo() {
           src="https://image.yes24.com/goods/112013526/XL"
           alt="bookImg"
         />
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <BookInfoTitle>니체의 말</BookInfoTitle>
           <BookInfoGenre>책 장르가 들어갈 자리 </BookInfoGenre>
-          <span>별점이 들어갈 자리</span>
+          <StarNumbers/>
           <Buttons>
             <form>
               <Button
                 style={{
                   border: "1px solid #888888",
                   backgroundColor: "#ffffff",
+                  padding: "0.8rem 1.1rem",
                 }}
               >
                 서재에 추가
               </Button>
               <Button
                 style={{
-                  border: "1px solid",
+                  border: "none",
                   backgroundColor: "#539165",
-                  color: "#ffffff",
                 }}
               >
-                기록하기
+                <Link to={"/record"} style={{ color: "#ffffff" }}>
+                  기록하기
+                </Link>
               </Button>
             </form>
           </Buttons>
@@ -41,23 +45,25 @@ export default function BookDetailinfo() {
 }
 export const BookDetailinfoDiv = styled.div`
   border-bottom: 0.06rem solid;
-  `;
+`;
 const BookInfos = styled.div`
   display: flex;
-  `;
+`;
 const BookInfoImg = styled.img`
   width: 14rem;
   height: 20rem;
   margin: 0 3rem 4rem 5rem;
 `;
 const Buttons = styled.div`
-  margin: 4rem 1rem 1rem;
-`
+  margin: 5rem 1rem 1rem;
+`;
 const Button = styled.button`
-  margin-right: 1rem; 
+  margin-right: 1rem;
   padding: 0.8rem 1.7rem;
   font-size: 1rem;
   border-radius: 7px;
   cursor: pointer;
-
+`;
+const StarNumbers = styled(StarNumber)`
+  margin: 0;
 `
