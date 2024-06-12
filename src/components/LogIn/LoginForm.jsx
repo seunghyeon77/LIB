ca76ae251a4089020166ec98ed10de02dbd87776
221186 <jdyjsh77@naver.com>
2024-06-12 15:30:26 +0900

    feat: 정보 수정 페이지 추가

--- src/components/LogIn/LoginForm.jsx
+++ src/components/LogIn/LoginForm.jsx
@@ -7,32 +7,33 @@ import {
   LoginFormInput,
   LoginFormTitle,
   LoginFormTitles,
+  SignupTextDiv,
 } from "../../styles/Login/LoginForm";
 
 export default function LoginForms() {
   return (
-    <LoginFormDiv>
+    <LoginFormDiv style={{height: '370px'}}>
       <LoginFormTitles>
         <LoginFormTitle>로그인</LoginFormTitle>
         <LoginFormContent>여러분들만의 서재를 만들어보세요.</LoginFormContent>
       </LoginFormTitles>
       <LoginForm>
-        <label htmlFor="formID">아이디</label>
-        <LoginFormInput type="text" placeholder="아이디 입력" id="formId" />
-        <label htmlFor="formPass">비밀번호</label>
+        <label>아이디</label>
+        <LoginFormInput type="text" placeholder="아이디 입력"/>
+        <label>비밀번호</label>
         <LoginFormInput
           type="password"
           placeholder="비밀번호 입력"
-          id="formPass"
         />
+
         <LoginFormButton style={{margin: '0.5rem 0'}}>로그인</LoginFormButton>
-        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <SignupTextDiv style={{display: 'flex'}}>
           <Link to={"/sign-up"}>
             <span style={{ color: "#000000", cursor: "pointer" }}>
               회원가입을 안 하셨다면
             </span>
           </Link>
-        </div>
+        </SignupTextDiv>
       </LoginForm>
     </LoginFormDiv>
   );
