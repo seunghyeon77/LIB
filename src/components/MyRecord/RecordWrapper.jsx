07007fae162900a3137882e8622abb70840562f6
221186 <jdyjsh77@naver.com>
2024-07-01 18:34:13 +0900

    style: 기록 컴포넌트 재배치

--- src/components/MyRecord/RecordWrapper.jsx
+++ src/components/MyRecord/RecordWrapper.jsx
@@ -1,25 +1,14 @@
 import styled from "styled-components";
 import Record from "./Record";
-import { MoreButton, MoreButtonForm } from "./MyStudyWrapper";
 
 export default function RecordWrapper() {
   return (
-    <div style={{margin: '1.7rem 0'}}>
+    <div style={{ margin: "1.7rem 0" }}>
       <RecordsWrapper>
-        <RecordTitle>현재 개의 기록을 작성했습니다.</RecordTitle>
+        <RecordTitle>현재 {}개의 기록을 작성했습니다.</RecordTitle>
         <Records>
           <Record />
-          <Record />  
-          <Record />
-          <Record />
-          <Record />
-          <Record />
-          <Record />
-          <Record />
         </Records>
-        <MoreButtonForm>
-          <MoreButton style={{margin: '2rem 0'}}>더 보기</MoreButton>
-        </MoreButtonForm>
       </RecordsWrapper>
     </div>
   );
@@ -33,9 +22,7 @@ const RecordsWrapper = styled.div`
   height: 790px; // 46rem
   background-color: #ffffff;
   border-radius: 7px;
-  `;
+`;
 const Records = styled.div`
   margin: 0 4.8rem;
-  display: flex;
-  flex-wrap: wrap;
 `;
