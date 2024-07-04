ee565603410f6a97fc7da3e261f720a0d06a91de
221186 <jdyjsh77@naver.com>
2024-07-04 15:15:20 +0900

    style: 컴포넌트 크기 조정

--- src/components/Main/BookWrapperBest.jsx
+++ src/components/Main/BookWrapperBest.jsx
@@ -3,7 +3,7 @@ import { Book, BookInfos, Img, TitleText } from "../../styles/Main/BookStyle";
 
 export default function BookWrapperBest() {
   return (
-    <Book>
+    <Book style={{margin: '5rem 0'}}>
       <TitleText>
         <Img src="https://github.com/JNU-econovation/LIB_FE/assets/144216731/2ac11d14-7f27-4a24-89cb-5f0e251afa68" />
         독자들이 좋아하는 책
