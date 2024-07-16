325b06e3806b691e1ba8184fae1d88328acc7e80
221186 <jdyjsh77@naver.com>
2024-07-16 23:39:58 +0900

    feat: aws s3 이미지 업로드

---
+++ src/api/awsImages.js
@@ -0,0 +1,10 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({
+  region: import.meta.env.VITE_AWS_REGION,
+  bucket: import.meta.env.VITE_S3_BUCKET_NAME,
+  credentials: {
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+  },
+});
--- src/components/Record/Bookimg.jsx
+++ src/components/Record/Bookimg.jsx
@@ -1,41 +1,62 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "../../api/awsImages";
 
-export default function Bookimg() {
-  const [previewImg, setPreviewImg] = useState(""); // UI적으로 보여줄 미리보기 img
+export default function Bookimg({ setBookCover, bookCover }) {
+  const [isLoading, setIsLoading] = useState(false);
 
-  const previewImgfunc = (e) => {
-    const fileImg = e.target.files[0];
-    const fileRead = new FileReader();
-    fileRead.onload = () => {
-      setPreviewImg(fileRead.result);
+  const reImgSelect = () => {
+    setBookCover("");
+  };
+
+  const handleUpload = async (file) => {
+    setIsLoading(true);
+
+    const params = {
+      Bucket: import.meta.env.VITE_S3_BUCKET_NAME,
+      Key: `${file.name}`,
+      Body: file,
+      ACL: "public-read",
     };
 
-    if (fileImg) {
-      fileRead.readAsDataURL(fileImg);
+    try {
+      const command = new PutObjectCommand(params);
+      const response = await s3Client.send(command);
+      console.log("사진 업로드 성공", response);
+
+      const imageUrl = `https://${params.Bucket}.s3.amazonaws.com/${params.Key}`;
+      setBookCover(imageUrl);
+      setIsLoading(false);
+    } catch (err) {
+      console.error("Error uploading file:", err);
+      setIsLoading(false);
     }
   };
 
-  const reImgSelect = () => {
-    setPreviewImg("");
-  };
-  
   return (
     <AddFile>
-      {previewImg ? (
-        <Img src={previewImg} alt="preview 이미지" onClick={reImgSelect} />
+      {isLoading ? (
+        <AddFileText>사진 추가 중...</AddFileText>
       ) : (
-        <AddFileText htmlFor="addFile">사진 추가</AddFileText>
+        <>
+          {bookCover ? (
+            <Img src={bookCover} alt="책 이미지" onClick={reImgSelect} />
+          ) : (
+            <AddFileText htmlFor="addFile">사진 추가</AddFileText>
+          )}
+          <InputFile
+            type="file"
+            id="addFile"
+            accept="image/*"
+            onChange={(e) => handleUpload(e.target.files[0])}
+          />
+        </>
       )}
-      <InputFile
-        type="file"
-        id="addFile"
-        accept="image/*"
-        onChange={previewImgfunc}
-      />
     </AddFile>
   );
 }
+
 const AddFile = styled.div`
   width: 10.5rem;
   height: 13.5rem;
@@ -43,7 +64,6 @@ const AddFile = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
 `;
 const AddFileText = styled.label`
   opacity: 0.6;
