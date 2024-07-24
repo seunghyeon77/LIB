8ba13e50a5f788dae416b8262958d34babb726a7
221186 <jdyjsh77@naver.com>
2024-07-24 16:11:04 +0900

    feat: baseURL env파일 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,23 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+
 export const axiosInstance = axios.create({
-  baseURL: "https://6a4f-168-131-35-101.ngrok-free.app",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
-    'Access-Control-Allow-Credentials': true,
-    'ngrok-skip-browser-warning': true,
+    // "withCredentials": true,
+    "Access-Control-Allow-Credentials": true,
+    "ngrok-skip-browser-warning": true,
+    // "Accept": "application/json, text/plain",
   },
-})
+  mode: "cors"
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middleware: [
-      "/books",
-      {
-        target: "https://6a4f-168-131-35-101.ngrok-free.app",
-        changeOrigin: true,
-        pathRewrite: {
-          "^/books": "", 
-        },
-      },
-    ],
+  define: {
+    global: {},
   },
 });
+
